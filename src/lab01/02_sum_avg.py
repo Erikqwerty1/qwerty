8c6289a4d839f18e62bb466a711e6f86e1bd4900
unknown <erikmufazalov@gmail.com>
2025-09-18 00:57:44 +0300

    Любое сообщение

---
+++ src/lab01/02_sum_avg.py
@@ -0,0 +1,6 @@
+
+a = float(input(("Введите первое число=")))
+b = float(input(("Введите первое число=")))
+sum=a+b
+avg = sum/2
+print("{0:.2f}".format(sum,avg))
---
+++ src/lab01/05_initials_and_len.py
@@ -0,0 +1,4 @@
+fio = input().split()
+print(fio)
+print("Инициалы=",fio[0][0],fio[1][0],fio[2][0],sep='')
+print(len(fio[0]+fio[1]+fio[2])+2)
